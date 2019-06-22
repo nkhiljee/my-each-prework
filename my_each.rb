762f78a859668f614e3223b8656a17ef1ffe2ce7
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-22 18:52:50 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,14 @@
-def my_each(array)
-  i = 0
+def hello_t(array)
+  if block_given?
+    i = 0
  
-  while i < array.length
-    yield array[i]
-    i = i + 1
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
   end
 end
