14eae7677e98b48efefc6662f18464b33656ebaa
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-22 18:47:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) 
+  i = 0
+  while i < array.length
+  binding.pry
+    yield my_each(array) do |array|
+     # binding.pry
+    array[i]
+    i += 1
+    end
+    array
+  end
 end
